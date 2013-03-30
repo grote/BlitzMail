325d89939d47bdf0e13623c663e86c5bd26058c1
Torsten Grote <t@grobox.de>
2013-03-30 13:49:07 +0100

    password encryption uses private key now

--- README.md
+++ README.md
@@ -4,12 +4,24 @@ Share content via email with just one click!
 
 BlitzMail is an Android app that allows you to set up your email account once and then use it to send emails to an address of your choice. This comes in handy when you need to send a lot of things via email, because you are in a low connectivity area (e.g. subway) and want to remember things to look at later.
 
+The SMTP password is stored encrypted with a built-in key and salted with your device ID. This is not fully secure, but should provide reasonable protection for your password.
+
 Screenshots
 -----------
 
 ![BlitzMail Settings](/artwork/MainActivity.png)
 ![BlitzMail Share Via](/artwork/ShareVia.png)
 ![BlitzMail Sending Mail](/artwork/SendMailActivity.png)
+
+Building from Source
+--------------------
+Before building BlitzMail from source, make sure to run the following command.
+
+```bash
+cp src/de/grobox/blitzmail/PrivateConstants.sample_java src/de/grobox/blitzmail/PrivateConstants.java
+```
+
+Then edit `src/de/grobox/blitzmail/PrivateConstants.java` and replace `[Please insert your encryption key here]` with the encryption key of your choice.
 
 License
 -------
--- src/de/grobox/blitzmail/Crypto.java
+++ src/de/grobox/blitzmail/Crypto.java
@@ -14,7 +14,6 @@ public class Crypto {
 
 	private Context context;
 	protected static final String UTF8 = "utf-8";
-	private static final char[] SEKRIT = "testpassphrase".toCharArray();
 	
 	public Crypto(Context context) {
 		this.context = context;
@@ -25,7 +24,7 @@ public class Crypto {
         try {
             final byte[] bytes = value!=null ? value.getBytes(UTF8) : new byte[0];
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
-            SecretKey key = keyFactory.generateSecret(new PBEKeySpec(SEKRIT));
+            SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PrivateConstants.CryptoKey));
             Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
             pbeCipher.init(Cipher.ENCRYPT_MODE, key, new PBEParameterSpec(Settings.Secure.getString(context.getContentResolver(),Settings.System.ANDROID_ID).getBytes(UTF8), 20));
             return new String(Base64.encode(pbeCipher.doFinal(bytes), Base64.NO_WRAP),UTF8);
@@ -41,7 +40,7 @@ public class Crypto {
         try {
             final byte[] bytes = value!=null ? Base64.decode(value,Base64.DEFAULT) : new byte[0];
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
-            SecretKey key = keyFactory.generateSecret(new PBEKeySpec(SEKRIT));
+            SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PrivateConstants.CryptoKey));
             Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
             pbeCipher.init(Cipher.DECRYPT_MODE, key, new PBEParameterSpec(Settings.Secure.getString(context.getContentResolver(),Settings.System.ANDROID_ID).getBytes(UTF8), 20));
             return new String(pbeCipher.doFinal(bytes),UTF8);
